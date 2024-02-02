9e8602e709a6acb09158c3f5e6bedc064fb4a183
thanhnd2929 <thanhnd2412@gmail.com>
2024-02-02 23:21:31 +0700

    first commit

---
+++ app/src/main/java/fpoly/thanhndph45160/nhom6_quanlithuchi/MainActivity.java
@@ -0,0 +1,14 @@
+package fpoly.thanhndph45160.nhom6_quanlithuchi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
